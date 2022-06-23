a56167a43f7b283f394d272cdaf59e47f0d1b8ff
alex <alextar994@gmail.com>
2022-06-23 14:20:32 +0300

    Выполни 4 шаг

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -2,12 +2,13 @@
 
 require_relative 'hexlet_code/version'
 
+# This module generate tags
 module HexletCode
-  def self.form_for(object, attributes = {}, &block)
+  def self.form_for(_object, attributes = {}, &block)
     url = attributes[:url] || '#'
     Tag.build('form', action: url, method: 'post', &block)
   end
-  
+
   # This class help to build tags.
   class Tag
     SINGLE_TAGS = %w[br input img link source].freeze
